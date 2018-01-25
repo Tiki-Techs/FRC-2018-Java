dcd1e9644a187224f687c7c661dd67276781636e
OliverGray <jmandrake42@gmail.com>
2018-01-24 17:14:01 -1000

    full build

--- src/frc/team3880/Robot.java
+++ src/frc/team3880/Robot.java
@@ -1,134 +1,124 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.team3880;
+package frc.team3880.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.team3880.commands.ExampleCommand;
-import frc.team3880.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
+ * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
  */
-// If you rename or move this class, update the build.properties file in the project root
-public class Robot extends TimedRobot 
-{
 
-    public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-    public static OI oi;
 
-    private Command autonomousCommand;
-    private SendableChooser<Command> chooser = new SendableChooser<>();
+public class Robot extends IterativeRobot{
+    Joystick leftJoy = new Joystick(0);
+    Button 	button1 = new JoystickButton(leftJoy, 1),
+            button2 = new JoystickButton(leftJoy, 2),
+            button3 = new JoystickButton(leftJoy, 3),
+            button4 = new JoystickButton(leftJoy, 4),
+            button5 = new JoystickButton(leftJoy, 5),
+            button6 = new JoystickButton(leftJoy, 6),
+            button7 = new JoystickButton(leftJoy, 7),
+            button8 = new JoystickButton(leftJoy, 8);
+    Victor victorZero = new Victor(0);
+    Victor victorOne = new Victor(1);
+    Victor victorTwo = new Victor(2);
+    Victor victorThree = new Victor(3);
+
+
+    private Timer m_timer = new Timer();
 
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     @Override
-    public void robotInit() 
-    {
-        oi = new OI();
-        chooser.addDefault("Default Auto", new ExampleCommand());
-        // chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
+    public void robotInit() {
     }
 
     /**
-     * This function is called once each time the robot enters Disabled mode.
-     * You can use it to reset any subsystem information you want to clear when
-     * the robot is disabled.
+     * This function is run once each time the robot enters autonomous mode.
      */
     @Override
-    public void disabledInit() 
-    {
-        
-    }
-
-    @Override
-    public void disabledPeriodic() 
-    {
-        Scheduler.getInstance().run();
-    }
-
-    /**
-     * This autonomous (along with the chooser code above) shows how to select
-     * between different autonomous modes using the dashboard. The sendable
-     * chooser code works with the Java SmartDashboard. If you prefer the
-     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-     * getString code to get the auto name from the text box below the Gyro
-     *
-     * <p>You can add additional auto modes by adding additional commands to the
-     * chooser code above (like the commented example) or additional comparisons
-     * to the switch structure below with additional strings & commands.
-     */
-    @Override
-    public void autonomousInit() 
-    {
-        autonomousCommand = chooser.getSelected();
-
-        /*
-         * String autoSelected = SmartDashboard.getString("Auto Selector",
-         * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-         * = new MyAutoCommand(); break; case "Default Auto": default:
-         * autonomousCommand = new ExampleCommand(); break; }
-         */
-
-        // schedule the autonomous command (example)
-        if (autonomousCommand != null) 
-        {
-            autonomousCommand.start();
-        }
+    public void autonomousInit() {
+        m_timer.reset();
+        m_timer.start();
     }
 
     /**
      * This function is called periodically during autonomous.
      */
     @Override
-    public void autonomousPeriodic() 
-    {
-        Scheduler.getInstance().run();
-    }
-
-    @Override
-    public void teleopInit() 
-    {
-        // This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-        if (autonomousCommand != null) 
-        {
-            autonomousCommand.cancel();
+    public void autonomousPeriodic() {
+        while(m_timer.get() < 2) {
+            victorZero.set(1);
+            victorOne.set(1);
+            victorTwo.set(1);
+            victorThree.set(1);
+        }
+        while(m_time.get() < 2) {
+            victorZero.set(1);
+            victorOne.set(1);
+            victorTwo.set(-1);
+            victorThree.set(-1);
         }
     }
 
     /**
-     * This function is called periodically during operator control.
+     * This function is called once each time the robot enters teleoperated mode.
      */
     @Override
-    public void teleopPeriodic() 
-    {
-        Scheduler.getInstance().run();
+    public void teleopInit() {
+        victorZero.set(0);
+        victorOne.set(0);
+        victorTwo.set(0);
+        victorThree.set(0);
+    }
+    public Command motorControl(int value, Victor motor) {
+        motor.set(value);
+        return null;
+    }
+    /**
+     * This function is called periodically during teleoperated mode.
+     */
+    @Override
+    public void teleopPeriodic() {
+        while(button1.get()) {
+            victorOne.set(1);
+        }
+
+        if (!button1.get()) {
+            victorOne.set(0);
+        }
+
+
+        victorZero.set(leftJoy.getY() + leftJoy.getX());
+        victorOne.set(leftJoy.getY() + leftJoy.getX());
+        victorTwo.set(-leftJoy.getY() + leftJoy.getX());
+        victorThree.set(-leftJoy.getY() + leftJoy.getX());
     }
 
     /**
      * This function is called periodically during test mode.
      */
     @Override
-    public void testPeriodic() 
-    {
-        
+    public void testPeriodic() {
     }
 }
+
