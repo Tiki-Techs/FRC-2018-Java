77c6c39e28e557c33c3b15ddcdfa98081beccb88
lobrien <lobrien@knowing.net>
2018-03-20 19:36:13 -1000

    Cleanup AutoDriveForwardTurnRight

--- competitionBot/src/org/usfirst/frc/team3880/robot/commands/autonomous/AutoDriveForwardTurnRight.java
+++ competitionBot/src/org/usfirst/frc/team3880/robot/commands/autonomous/AutoDriveForwardTurnRight.java
@@ -1,14 +1,14 @@
 package org.usfirst.frc.team3880.robot.commands.autonomous;
 
-import org.usfirst.frc.team3880.robot.commands.CommandBase;
-import edu.wpi.first.wpilibj.Timer;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.Timer;
+import org.usfirst.frc.team3880.robot.commands.CommandBase;
 
 public class AutoDriveForwardTurnRight extends CommandBase {
 	Timer timer;
-	
+
 	double DRIVE_VALUE = 0.1;
+
 	public AutoDriveForwardTurnRight() {
 		requires(drive);
 		requires(lift);
@@ -17,52 +17,48 @@ public class AutoDriveForwardTurnRight extends CommandBase {
 		requires(rightIntakeWheel);
 		requires(intakeArms);
 	}
-	
-	 @Override
-	 protected void initialize() {
-		 timer  = new Timer();
-	    	timer.reset();
-	    	timer.start();
-	 }
+
+	@Override
+	protected void initialize() {
+		timer = new Timer();
+		timer.reset();
+		timer.start();
+	}
 
 
-	 @Override
-	 protected void execute() {
-		 System.out.println(timer.get());
-		
-		 if(timer.get() < 0.5) {
-			 windowMotor.set(-1);
-		 }
-		 else if(timer.get() < 4.5) {
-			 drive.set(-.4, -.4);
-		 }
-		 else if(timer.get() < 5.5){
-			 drive.set(.4,  -.4);
-			 lift.set(.7);
-		 }
-		 else if(timer.get() < 6){
-			 drive.set(-.4, -.4);
-		 }
-		 else if(timer.get() < 6.5) {
-			 rightIntakeWheel.spin(-1);
-			 leftIntakeWheel.spin(1);
-		 }
-		
-	 }
+	@Override
+	protected void execute() {
+		System.out.println(timer.get());
 
-	 @Override
-	 protected void end() {
-		 drive.backLeftDrive.set(ControlMode.PercentOutput, 0);
-		 drive.frontLeftDrive.set(ControlMode.PercentOutput, 0);
-		 drive.backRightDrive.set(ControlMode.PercentOutput, 0);
-		 drive.frontRightDrive.set(ControlMode.PercentOutput, 0);
-	 }
+		if (timer.get() < 0.5) {
+			windowMotor.set(-1);
+		} else if (timer.get() < 4.5) {
+			drive.set(-.4, -.4);
+		} else if (timer.get() < 5.5) {
+			drive.set(.4, -.4);
+			lift.set(.7);
+		} else if (timer.get() < 6) {
+			drive.set(-.4, -.4);
+		} else if (timer.get() < 6.5) {
+			rightIntakeWheel.spin(-1);
+			leftIntakeWheel.spin(1);
+		}
 
-	 @Override
-	 protected void interrupted() {
-	 	end();
-	 }
-	    
+	}
+
+	@Override
+	protected void end() {
+		drive.backLeftDrive.set(ControlMode.PercentOutput, 0);
+		drive.frontLeftDrive.set(ControlMode.PercentOutput, 0);
+		drive.backRightDrive.set(ControlMode.PercentOutput, 0);
+		drive.frontRightDrive.set(ControlMode.PercentOutput, 0);
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
