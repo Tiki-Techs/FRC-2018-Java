44de8912595f97fac919e5a37c665dbbfc010910
lobrien <lobrien@knowing.net>
2018-03-20 19:46:52 -1000

    Auto_DriveForwardAndTurnRight : Total guess, but dashboarded
    By changing `autoFTR1FinalTime`, `autoFTR1LeftPct` and `autoFTR1RightPct` — you should be able to change the turn

--- competitionBot/src/org/usfirst/frc/team3880/robot/commands/autonomous/AutoDriveForwardTurnRight.java
+++ competitionBot/src/org/usfirst/frc/team3880/robot/commands/autonomous/AutoDriveForwardTurnRight.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3880.robot.commands.autonomous;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team3880.robot.commands.CommandBase;
 
 public class AutoDriveForwardTurnRight extends CommandBase {
@@ -29,16 +30,28 @@ public class AutoDriveForwardTurnRight extends CommandBase {
 	@Override
 	protected void execute() {
 		System.out.println(timer.get());
+		double step0FinalTime = SmartDashboard.getNumber("autoFTR0FinalTime", 4.5);
+		double step0LeftPct = SmartDashboard.getNumber("autoFTR0LeftPct", -0.4);
+		double step0RighttPct = SmartDashboard.getNumber("autoFTR0RightPct", -0.4);
+
+		double step1FinalTime = SmartDashboard.getNumber("autoFTR1FinalTime", 5.5);
+		double step1LeftPct = SmartDashboard.getNumber("autoFTR1LeftPct", -0.4);
+		double step1RighttPct = SmartDashboard.getNumber("autoFTR1RightPct", 0.4);
+		double step1LiftPct = SmartDashboard.getNumber("autoFTR1RLiftPct", 0.7);
+
+		double step2FinalTime = SmartDashboard.getNumber("autoFTR2FinalTime", 6);
+		double step2LeftPct = SmartDashboard.getNumber("autoFTR2LeftPct", -0.4);
+		double step2RighttPct = SmartDashboard.getNumber("autoFTR2RightPct", -0.4);
 
 		if (timer.get() < 0.5) {
 			windowMotor.set(-1);
-		} else if (timer.get() < 4.5) {
-			drive.set(-.4, -.4);
-		} else if (timer.get() < 5.5) {
-			drive.set(.4, -.4);
-			lift.set(.7);
-		} else if (timer.get() < 6) {
-			drive.set(-.4, -.4);
+		} else if (timer.get() < step0FinalTime) {
+			drive.set(step0LeftPct, step0RighttPct);
+		} else if (timer.get() < step1FinalTime) {
+			drive.set(step1LeftPct, step1RighttPct);
+			lift.set(step1LiftPct);
+		} else if (timer.get() < step2FinalTime) {
+			drive.set(step2LeftPct, step2RighttPct);
 		} else if (timer.get() < 6.5) {
 			rightIntakeWheel.spin(-1);
 			leftIntakeWheel.spin(1);
